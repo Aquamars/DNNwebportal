e3c9ad0067cf7818b9c032e46ad41804e93392ea
A40503 <itriA40503@itri.org.tw>
2017-04-21 16:47:13 +0800

    add app.js

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Main from './Main'; // Our custom react component
+import { I18nextProvider } from 'react-i18next';
+import i18n from './utils/i18n';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// Render the main app react component into the app div.
+// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+render(<I18nextProvider i18n={ i18n }><Main /></I18nextProvider>, document.getElementById('app'));
