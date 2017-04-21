3cc1a43260ffa130b1a3d1e196dc205c3d81e767
A40503 <itriA40503@itri.org.tw>
2017-04-21 16:43:48 +0800

    add dummy data

---
+++ src/app/resource/index.js
@@ -0,0 +1,9 @@
+export const DOMAIN = 'http://tarsanad.ddns.net'
+export const PORT = '9527'
+export const PATH = 'web'
+export const API_URL = DOMAIN+':'+PORT+'/'+PATH+'/'
+
+export const DATA = [
+{startTime:'2017-01-02',endTime:'2017-05-02', machine:'eeny', status:'running', image:'c2c3152907b5', project:'G352BQ2100', account:'information', password:'research'},
+{startTime:'2017-03-22',endTime:'2017-06-26', machine:'meeny', status:'stop', image:'fb434121fc77', project:'G352BQ2100', account:'communications', password:'research'},
+{startTime:'2017-08-08',endTime:'2017-09-12', machine:'minyMoe', status:'initial', image:'91c95931e552', project:'G352BQ2100', account:'laboratories', password:'research'},]
