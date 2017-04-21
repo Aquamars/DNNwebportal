45ae2a50a3405d96d733084fdba6a3c0875d9513
A40503 <itriA40503@itri.org.tw>
2017-04-21 16:41:36 +0800

    webpack setting

---
+++ webpack-dev-server.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'build');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+const config = {
+  // Entry points to the project
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/src/app/app.js'),
+  ],
+  // Server Configuration options
+  devServer: {
+    contentBase: 'src/www', // Relative directory for base of server
+    devtool: 'eval',
+    hot: true, // Live-reload
+    inline: true,
+    port: 3000, // Port Number
+    host: '0.0.0.0', // Change to '0.0.0.0' for external facing server
+  },
+  devtool: 'eval',
+  output: {
+    path: buildPath, // Path of output file
+    filename: 'app.js',
+  },
+  plugins: [
+    // Enables Hot Modules Replacement
+    new webpack.HotModuleReplacementPlugin(),
+    // Allows error warnings but does not stop compiling.
+    new webpack.NoErrorsPlugin(),
+    // Moves files
+    new TransferWebpackPlugin([
+      {from: 'www'},
+    ], path.resolve(__dirname, 'src')),
+
+    new webpack.IgnorePlugin(/\.\/locale$/)
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // All .js files
+        loaders: ['babel-loader'],
+        exclude: [nodeModulesPath],
+      },
+      { test: /\.less/, loader: 'style-loader!css-loader!less-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        loader:'style!css!'
+      }
+    ],
+  },
+};
+
+module.exports = config;
