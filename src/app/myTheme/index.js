5a1b395d6d2531f6f7d3e0b9f7553f23d4c31e20
A40503 <itriA40503@itri.org.tw>
2017-04-28 16:30:01 +0800

    add muitheme

---
+++ src/app/myTheme/index.js
@@ -0,0 +1,43 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { 
+	lightBlue500, 
+	lightBlue900, 
+	blueA400, 
+	orangeA700, 
+	redA700, 
+	greenA700,
+	green500,
+	pink500,
+	white,
+	darkBlack,
+	fullBlack,
+} from 'material-ui/styles/colors'
+
+export const muiStyle = {
+  palette: {
+    primary1Color: lightBlue900,
+    primary2Color: lightBlue500,    
+    accent1Color: pink500,
+    accent2Color: redA700,
+    textColor: darkBlack,    
+  },
+}
+
+export const muiTheme = getMuiTheme({
+  tabs: {
+      backgroundColor: 'white',
+      selectedTextColor: muiStyle.palette.primary1Color,
+      textColor: '#757575',
+  },
+  inkBar: {
+      backgroundColor: muiStyle.palette.primary1Color,
+  },
+  datePicker: { 
+    selectColor: muiStyle.palette.primary1Color,
+    selectTextColor: 'white',
+  },
+  stepper: {
+      backgroundColor: 'transparent',
+      iconColor: muiStyle.palette.primary1Color,      
+  },
+})
