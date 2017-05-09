d00af737402faea8aea99f372634b38bd8723449
A40503 <itriA40503@itri.org.tw>
2017-05-09 15:02:41 +0800

    add loading to handleSubmit

--- src/app/components/EditModal.js
+++ src/app/components/EditModal.js
@@ -9,6 +9,7 @@ import ReactTooltip from 'react-tooltip'
 import moment from 'moment'
 import ReviewCalendar from './ReviewCalendar'
 import IconButton from 'material-ui/IconButton'
+import CircularProgress from 'material-ui/CircularProgress'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 // ICON
 import EditorModeEdit from 'material-ui/svg-icons/editor/mode-edit'
@@ -20,6 +21,9 @@ import { blueA400, green500, pink500 } from 'material-ui/styles/colors'
 // i18n
 import { translate, Interpolate } from 'react-i18next'
 import i18n from '../utils/i18n'
+// API call
+import axios from 'axios'
+import {API_DeleteSchedule} from '../resource'
 /**
  * A modal dialog can only be closed by selecting one of the actions.
  */
@@ -29,18 +33,45 @@ class EditModal extends React.Component {
       super(props)
       this.state = {
         open: false,
-        increaseDay:0 
+        increaseDay:0,
+        loading: false,
+        comfirm: false
       }
   }
-
+  dummyAsync = (cb) => {
+      this.setState({loading: true}, () => {
+        this.asyncTimer = setTimeout(cb, 800);
+     })
+  }
   handleOpen = () => {
     this.setState({open: true})
   }
-
   handleClose = () => {
     this.setState({open: false});
   }
-
+  handleSubmit = () => {
+    if(!this.state.comfirm){
+      this.setState({
+        loading: true,
+      })
+      axios.get(
+        API_DeleteSchedule,
+        {}
+      ).then((result)=>{
+        console.log(result.data)
+        this.dummyAsync(()=>this.setState({
+          loading: false,
+          comfirm: true
+        }))        
+      }).catch((err)=>{
+          console.log(err)
+      })
+    }else{
+      console.log('refresh')
+      this.setState({open: false, comfirm: false})
+      this.props.refresh()
+    }
+  }
   handleChangeMaxDate = (event, date) => {
     // console.log(date)    
     this.setState({
@@ -48,23 +79,23 @@ class EditModal extends React.Component {
       increaseDay: moment(date).diff(moment(this.props.data.endTime), 'days')
     })
   }
-
   disableDate = (date) => {    
     return moment(date).isBefore(this.props.data.endTime) || moment(date).isAfter(moment(this.props.data.endTime).add(1, 'month'))
   }
-
   render() {
     const {t} = this.props
     const actions = [
       <FlatButton
         label={t('common:cancel')}
         primary={true}
+        disabled={this.state.comfirm || this.state.loading}
         onTouchTap={this.handleClose}
       />,
       <FlatButton
         label={t('common:submit')}
         secondary={true}
-        onTouchTap={this.handleClose}
+        disabled={this.state.loading || this.state.increaseDay<=0}
+        onTouchTap={this.handleSubmit}
       />,
     ]
     const optionsStyle = {
@@ -89,7 +120,12 @@ class EditModal extends React.Component {
             modal={true}
             open={this.state.open}
           >
+          {this.state.comfirm ?
+            <div><b>Already Updated !</b></div> :
             <div style={optionsStyle}>
+              {this.state.loading ? 
+                <div style = {{textAlign:'center'}}><CircularProgress size={80} thickness={5} /></div> :
+              <div>
               <Divider />
               <TextField
                 disabled={true}
@@ -105,7 +141,10 @@ class EditModal extends React.Component {
               />
               {this.state.increaseDay > 0 && <span>{moment(this.props.data.endedAt).format('YYYY-MM-DD')} <font color={green500}>+ {this.state.increaseDay} {t('common:days')}</font></span>}
               <ReviewCalendar />
+              </div>
+              }
             </div>
+          }
           </Dialog>
         </div>
       </MuiThemeProvider>
