0eb6a3047fa79bdd74a7379b78635026c543778f
A40503 <itriA40503@itri.org.tw>
2017-07-17 15:26:32 +0800

    add chart - radar

---
+++ src/app/components/Charts/Radar.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import RC2 from 'react-chartjs2';
+import { Card, CardTitle } from 'material-ui/Card';
+const data = {
+  labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(179,181,198,0.2)',
+      borderColor: 'rgba(179,181,198,1)',
+      pointBackgroundColor: 'rgba(179,181,198,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(179,181,198,1)',
+      data: [65, 59, 90, 81, 56, 55, 40]
+    },
+    {
+      label: 'My Second dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      pointBackgroundColor: 'rgba(255,99,132,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(255,99,132,1)',
+      data: [28, 48, 40, 19, 96, 27, 100]
+    }
+  ]
+};
+
+class Radar extends Component {
+  render() {
+    return (
+      <Card style={{height:'100%'}}>
+        <CardTitle style={{padding:'10px'}} title="Radar Chart" />
+        <RC2 data={data} type='radar' />
+      </Card>
+    );
+  }
+}
+
+export default Radar;
