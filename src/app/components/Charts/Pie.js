e38e0cdce911c4a7d37a6ae8279584e3ec518f23
A40503 <itriA40503@itri.org.tw>
2017-07-17 15:27:29 +0800

    add chart - pie using 'react-chartjs2'

---
+++ src/app/components/Charts/Pie.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import RC2 from 'react-chartjs2';
+import { Card, CardTitle } from 'material-ui/Card'
+import {randomColors} from '../../utils/ColorHandler'
+
+const Orgdata = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [      
+      '#36A2EB',
+      '#FF6384',
+      '#FFCE56'
+    ],
+    hoverBackgroundColor: [      
+      '#36A2EB',
+      '#FF6384',
+      '#FFCE56'
+    ]
+  }]
+};
+
+class Pie extends Component {
+  render() {
+    let r = new randomColors()
+    let chartlabels = []
+    let chartData = []
+    let data = Orgdata
+    for(let key in this.props.data) { 
+        chartlabels.push(key)
+        chartData.push(this.props.data[key])
+    }
+    // console.log(chartlabels,chartData)
+    data.labels = chartlabels
+    data.datasets[0].data = chartData
+    console.log(this.props.title+':'+data.datasets[0].data)
+    // let newbackgroundColor = []
+    // data.datasets[0].data.map(()=>{
+    //   newbackgroundColor.push(r.get())
+    // })
+    // data.datasets[0].backgroundColor = newbackgroundColor
+    // data.datasets[0].hoverBackgroundColor = newbackgroundColor
+    
+    return (
+      <Card style={{height:'100%'}}>
+        <CardTitle style={{padding:'10px'}} title={this.props.title} />
+        <RC2 data={this.props.data} type='pie' />
+      </Card>
+    );
+  }
+}
+
+export default Pie;
