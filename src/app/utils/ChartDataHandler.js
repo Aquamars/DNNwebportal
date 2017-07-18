e579b84c49bfb03b803a4b31f7081c732fe7ead4
A40503 <itriA40503@itri.org.tw>
2017-07-18 10:58:51 +0800

    add toLineData

--- src/app/utils/ChartDataHandler.js
+++ src/app/utils/ChartDataHandler.js
@@ -37,7 +37,7 @@ export function toBarData (jsonData){
       dataset.data = chartData
 
       data.datasets.push(dataset)
-	  data.labels = chartlabels
+	    data.labels = chartlabels
 
     }
     return data
@@ -64,4 +64,53 @@ export function toPieDoughnutData (jsonData){
     data.datasets[0].hoverBackgroundColor = colors
     
     return data
+}
+
+export function toLineData (jsonData){
+  let data = {}
+  data.datasets = []
+  console.log(jsonData)
+  let r = new randomColors()
+  let dataset = {}
+  let chartData = [] 
+  let chartlabels = []
+  for(let key in jsonData) {
+    chartlabels.push(key)
+    chartData.push(jsonData[key])      
+  }
+      // console.log(chartData)
+  const Color = r.get()+''
+  const Color2 = r.get()+''
+  dataset.label = 'data'
+  dataset.fill = false
+  dataset.lineTension = 0.1
+  dataset.backgroundColor = hexToRGB(Color, 0.4)
+  dataset.borderColor = hexToRGB(Color, 1)
+  dataset.borderCapStyle = 'butt',
+  dataset.borderDash = [],
+  dataset.borderDashOffset = 0.0,
+  dataset.borderJoinStyle = 'miter',
+  dataset.pointBorderColor = hexToRGB(Color, 1)
+  dataset.pointBackgroundColor = '#fff',
+  dataset.pointBorderWidth = 1,
+  dataset.pointHoverRadius = 5,
+  dataset.pointHoverBackgroundColor = hexToRGB(Color, 1)
+  dataset.pointHoverBorderColor = hexToRGB(Color2, 1)
+  dataset.pointHoverBorderWidth = 2
+  dataset.pointRadius = 1
+  dataset.pointHitRadius = 10
+    // data: [65, 59, 80, 81, 56, 55, 40],
+    // dataset.label = key
+    // dataset.backgroundColor = hexToRGB(barColor, 1)
+    // dataset.borderColor = hexToRGB(barColor, 1)
+    // dataset.hoverBackgroundColor = hexToRGB(barColor, 0.4)
+    // dataset.hoverBorderColor = hexToRGB(barColor, 1)
+  dataset.data = chartData
+
+  data.datasets.push(dataset)
+  data.labels = chartlabels
+
+    
+  console.log(data)
+  return data
 }
