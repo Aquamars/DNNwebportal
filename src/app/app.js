5701b8982732167a33f86fecb8eaa9e1fbb8479d
A40503 <itriA40503@itri.org.tw>
2017-07-28 09:11:13 +0800

    add appContainer for hot loader

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+// import {render} from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './utils/i18n';
@@ -11,9 +13,41 @@ injectTapEventPlugin();
 
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(
-	<I18nextProvider i18n={ i18n }>
-		<Main />
-	</I18nextProvider>,
-	document.getElementById('app')
-);
+// render(
+// 	<AppContainer>
+// 		<I18nextProvider i18n={ i18n }>
+// 			<Main />
+// 		</I18nextProvider>
+// 	</AppContainer>,
+// 	document.getElementById('app')
+// );
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+    	<I18nextProvider i18n={ i18n }>
+			<Component  />
+		</I18nextProvider>
+	</AppContainer>,
+    document.getElementById('app')
+  )
+}
+
+render(Main)
+
+// if (module.hot) {
+//   module.hot.accept('./Main', () => { render(Main) })
+// }
+
+console.log('module.hot:'+module.hot)
+
+if(module.hot) {
+    module.hot.accept('./Main', () => {
+        const NextApp = require('./Main').default
+        render(NextApp)
+    })
+}
+ 
+// if(module.hot) {
+//     module.hot.accept();
+// }
