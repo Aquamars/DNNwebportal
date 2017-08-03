f31ee7f00f336b2c487ab27c00746cf700b0aeb3
A40503 <itriA40503@itri.org.tw>
2017-08-03 17:59:32 +0800

    add react-copy-to-clipboard

--- src/app/components/HoverDiv.js
+++ src/app/components/HoverDiv.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import FaEye from 'react-icons/lib/fa/eye'
+import CopyToClipboard from 'react-copy-to-clipboard'
 // i18n
 import { translate, Interpolate } from 'react-i18next'
 import i18n from '../utils/i18n'
@@ -22,11 +23,13 @@ class HoverDiv extends Component {
 	}
 	render(){
 		return(
-		<div 
-	        onMouseEnter={this.onMouseEnterHandler}
-	        onMouseLeave={this.onMouseLeaveHandler} >
-	        {this.state.hover ? <span>{this.props.account}<br/>{this.props.password}</span> : <FaEye/>}
-	    </div>
+			<CopyToClipboard text={this.state.hover && this.props.password}>
+				<div 
+		        onMouseEnter={this.onMouseEnterHandler}
+		        onMouseLeave={this.onMouseLeaveHandler} >
+		        {this.state.hover ? <span>{this.props.account}<br/>{this.props.password}</span> : <FaEye/>}		        
+		    	</div>
+	    	</CopyToClipboard>
 		)
 	}
 }
