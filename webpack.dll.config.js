3a6cdc57bf8bb68cf0d478c6381579f39c08f7b0
A40503 <itriA40503@itri.org.tw>
2017-08-09 15:32:36 +0800

    add module , add sematic-ui-css, semantic-ui-react

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -23,7 +23,9 @@ module.exports = {
           'i18next-browser-languagedetector',
           'i18next-xhr-backend',
           'react-virtualized-select',
-          'crypto-js'
+          'crypto-js',
+          'semantic-ui-css/semantic.min.css',
+          'semantic-ui-react'
 
         ],
     },
@@ -64,5 +66,50 @@ module.exports = {
             threshold: 10240,
             minRatio: 0.8
         }),
-    ]
+    ],
+    module: {
+    rules: [
+      {
+        test: /\.js$/, // All .js files
+        use: [
+       
+          'babel-loader'
+      
+        ], // react-hot is like browser sync and babel loads jsx and es6-7
+        exclude: /(node_modules)/,
+      },
+      { 
+        test: /\.less/,
+        use: [
+          
+            "style-loader",
+            "css-loader",
+            "less-loader"
+          
+        ],
+        exclude: /node_modules/ 
+      },
+      {
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: { url: false }
+            },
+        ],
+      },
+      //image package
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
+      }
+    ],
+  },
 };
