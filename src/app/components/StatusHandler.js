451cf6ef5cc97c26afe4bd21587c8bbb53418f82
A40503 <itriA40503@itri.org.tw>
2017-08-09 15:31:24 +0800

    add label for status

--- src/app/components/StatusHandler.js
+++ src/app/components/StatusHandler.js
@@ -9,6 +9,8 @@ import {
 	redA700
 } from 'material-ui/styles/colors'
 import React, { Component, PropTypes } from 'react'
+import { Label } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 // i18n
 import { translate, Interpolate } from 'react-i18next'
 import i18n from '../utils/i18n'
@@ -25,36 +27,54 @@ class StatusHandler extends Component {
 		let obj;
 		console.log('StatusHandler:'+status)
 		switch(status){
-			case 0:
-				obj = <font color ={yellow900}><b>{t('common:status.initial')}</b></font>
-				break
 			case 1:	
-			case '1':			
-				obj = <font color ={amber900}><b>{t('common:status.waiting')}</b></font>
+			case '1':
+				obj = <Label color={'yellow'}>
+				        {t('common:status.waiting')}
+				      </Label>
+				// obj = <font color ={amber900}><b>{t('common:status.waiting')}</b></font>
 				break
 			case 2:
 			case '2':
-				obj = <font color ={green700}><b>{t('common:status.loading')}</b></font>
+				obj = <Label color={'olive'}>
+				        {t('common:status.loading')}
+				      </Label>
+				// obj = <font color ={green700}><b>{t('common:status.loading')}</b></font>
 				break
 			case 3:
 			case '3':
-				obj = <font color ={greenA700}><b>{t('common:status.running')}</b></font>
+				obj = <Label color={'green'}>
+				        {t('common:status.running')}
+				      </Label>
+				// obj = <font color ={greenA700}><b>{t('common:status.running')}</b></font>
 				break
 			case 4:
 			case '4':
-				obj = <font color ={deepOrangeA400}><b>{t('common:status.deleting')}</b></font>
+				obj = <Label color={'orange'}>
+				        {t('common:status.deleting')}
+				      </Label>
+				// obj = <font color ={deepOrangeA400}><b>{t('common:status.deleting')}</b></font>
 				break
 			case 5:
 			case '5':
-				obj = <font color ={deepOrangeA700}><b>{t('common:status.deleted')}</b></font>
+				obj = <Label color={'pink'}>
+				        {t('common:status.deleted')}
+				      </Label>
+				// obj = <font color ={deepOrangeA700}><b>{t('common:status.deleted')}</b></font>
 				break
 			case 6:
 			case '6':
-				obj = <font color ={blue800}><b>{t('common:status.canceled')}</b></font>
+				obj = <Label color={'teal'}>
+				        {t('common:status.canceled')}
+				      </Label>
+				// obj = <font color ={blue800}><b>{t('common:status.canceled')}</b></font>
 				break
 			case 7:
 			case '7':
-				obj = <font color ={redA700}><b>{t('common:status.error')}</b></font>
+				obj = <Label color={'red'}>
+				        {t('common:status.error')}
+				      </Label>
+				// obj = <font color ={redA700}><b>{t('common:status.error')}</b></font>
 				break
 			default:
 				obj = <font color ={'#000'}><b>{'??????'}</b></font>
