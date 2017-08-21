ae63e08f777aae59f0acef992e362ee0f5d23d2d
A40503 <itriA40503@itri.org.tw>
2017-08-21 14:17:14 +0800

    add image index.js for code split

---
+++ src/app/image/index.js
@@ -0,0 +1,39 @@
+import _monkey from './rawImage.jpg'
+import _logo from './itri_EL_A.png'
+import _ftp from './tutorial/ftp.png'
+import _ftp2 from './tutorial/ftp2-2.png'
+import _webCreate1 from './tutorial/webCreate1-1.png'
+import _webCreate2 from './tutorial/webCreate2-1.png'
+import _webCreate3 from './tutorial/webCreate3-1.png'
+import _webCreate4 from './tutorial/webCreate4-1.png'
+import _webCreate5 from './tutorial/webCreate6.png'
+import _webCreate6 from './tutorial/webCreate7-1.png'
+import _webCreate7 from './tutorial/webCreate11.png'
+import _accessInstance1 from './tutorial/accessInstance1-2.png'
+import _accessInstance2 from'./tutorial/accessInstance5.png' 
+import _accessInstance3 from'./tutorial/accessInstance7.png' 
+import _tensorflow from './tutorial/tensorflow.png' 
+import _header from './tutorial/header2.png'
+import _think from './think.png'
+import _easterEgg from './op.jpg'
+import _DnnLogo from '../image/DNN Web logo_yellow.png'
+
+export const monkey = _monkey
+export const logo = _logo
+export const ftp = _ftp
+export const ftp2 = _ftp2
+export const webCreate1 = _webCreate1
+export const webCreate2 = _webCreate2
+export const webCreate3 = _webCreate3
+export const webCreate4 = _webCreate4
+export const webCreate5 = _webCreate5
+export const webCreate6 = _webCreate6
+export const webCreate7 = _webCreate7
+export const accessInstance1 = _accessInstance1
+export const accessInstance2 = _accessInstance2
+export const accessInstance3 = _accessInstance3
+export const tensorflow = _tensorflow
+export const header = _header
+export const empty = _think
+export const easterEgg = _easterEgg
+export const DnnLogo = _DnnLogo
