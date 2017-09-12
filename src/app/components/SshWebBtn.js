8c30e513405b8fe6fd2e9db076f48fe69e2f2bfa
A40503 <itriA40503@itri.org.tw>
2017-09-12 16:08:00 +0800

    add location on ssh web

--- src/app/components/SshWebBtn.js
+++ src/app/components/SshWebBtn.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import IconButton from 'material-ui/IconButton'
 import FlatButton from 'material-ui/FlatButton'
 import { indigo900 } from 'material-ui/styles/colors'
-import { SshWebURL } from '../resource'
+import { SshWebURL, SshWebHost } from '../resource'
 // ICON
 import Terminal from 'react-icons/lib/go/terminal'
 import { translate, Interpolate } from 'react-i18next'
@@ -34,14 +34,15 @@ class SshWebBtn extends Component {
 							<FlatButton
 								icon={<Terminal size={28} color='black'/>}
 								label={<span><font color={indigo900}><b>{'ssh From Web (unstable)'}</b></font></span>}
-								href={SshWebURL + this.props.data.instance.username + '@' + this.props.data.instance.ip + ':' + this.props.data.instance.port} 
+								href={SshWebURL + this.props.data.instance.username + '@' + this.props.data.instance.ip + ':' + this.props.data.instance.port + '&ssh_once=true' + '&location=' + this.props.data.id} 
 					   			target='_blank'
 							/>
 			    		</div>
 	    			: <div>
 	    				<FlatButton
 							icon={<Terminal size={28} color='black'/>}
-							label={<span><font color={indigo900}><b>{'ssh From Web (unstable)'}</b></font></span>}						
+							label={<span><font color={indigo900}><b>{'ssh From Web (unstable)'}</b></font></span>}
+							href={SshWebHost}				
 						/>
 	    			 </div>
 				}
