d6d476deb88a302e70bc887f68d3e08eac028785
A40503 <itriA40503@itri.org.tw>
2017-09-29 08:50:37 +0800

    reformat FtpPass

--- src/app/utils/FtpPass.js
+++ src/app/utils/FtpPass.js
@@ -1,7 +1,7 @@
-import CryptoJS from 'crypto-js'
+import CryptoJS from 'crypto-js';
 
 export const ftpPass = (username) => {
-	const md5Text = CryptoJS.MD5(username+'ITRIDNN').toString()
-	const pass = md5Text.slice(0,6)
-	return pass
-}
+  const md5Text = CryptoJS.MD5(username + 'ITRIDNN').toString();
+  const pass = md5Text.slice(0, 6);
+  return pass;
+};
